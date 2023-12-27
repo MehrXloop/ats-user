3380b76000d58ad4acf74320e4421fb10237249f
Mehrunissa Pirwani (2303.KHI.CND.007) <mehrunissa.pirwani@xloopdigital.com>
2023-12-27 19:04:35 +0500

    adding signup options component

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -2,9 +2,15 @@
 import React, { useState } from 'react'
 import dummyData, { Job } from '@/lib/jobs'
 import moment from 'moment';
+import SignUpOptions from '@/components/SignUpOptions';
+import { useSession } from 'next-auth/react';
 
 const Jobs = () => {
+  const {data :session } = useSession()
   const [selectedJob, setSelectedJob] = useState<Job>(dummyData[0]);
+  const [applyModal, setApplyModal] = useState(false)
+  const [showSignModal, setShowSignModal] = useState(false);
+
 
   const handleJobClick = (job: Job) => {
     setSelectedJob(job);
@@ -27,43 +33,73 @@ const Jobs = () => {
     }
   };
 
-
   return (
-    <div className='flex justify-between items-start mx-2'>
-      <div>
-        {dummyData.map((job: Job) => (
-          <div className='job_card glassmorphism' key={job.id}>
-            <div className='flex justify-between items-start gap-1 flex-col'>
-              <h1 className='font-bold text-2xl cursor-pointer' onClick={() => handleJobClick(job)}>{job.title}</h1>
-              <p className='text-xl'>{job.city}, {job.country}, {job.nature_of_job}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="job_card glassmorphism">
-        {selectedJob && (
-          <div>
-            <h1 className='font-bold text-3xl cursor-pointer'>{selectedJob.title}</h1>
-            <p className='text-xl'>{selectedJob.city}, {selectedJob.country}, {selectedJob.nature_of_job}</p>
-            <div className='flex items-center justify-between mb-4'>
-              <p className='text-xl'>Posted {formatPostedTime(selectedJob.postedAt)}</p>
-              <div className='flex gap-2'>
-                <button type='button' className='border_btn'>
-                  Share Job
-                </button>
-                <button type='button' className='border_btn' >
-                  Apply Now
-                </button>
+    <>
+      <div className='flex justify-between items-start mx-2'>
+        <div>
+          {dummyData.map((job: Job) => (
+            <div className='job_card glassmorphism' key={job.id}>
+              <div className='flex justify-between items-start gap-1 flex-col'>
+                <h1 className='font-bold text-2xl cursor-pointer' onClick={() => handleJobClick(job)}>{job.title}</h1>
+                <p className='text-xl'>{job.city}, {job.country}, {job.nature_of_job}</p>
               </div>
             </div>
-            <hr className='mb-4'/>
-            <h1 className='font-bold text-2xl'>Description</h1>
-            <p className='text-xl'>{selectedJob.description}</p>
-          </div>
-        )}
+          ))}
+        </div>
+        <div className="job_card glassmorphism">
+          {selectedJob && (
+            <div>
+              <h1 className='font-bold text-3xl cursor-pointer'>{selectedJob.title}</h1>
+              <p className='text-xl'>{selectedJob.city}, {selectedJob.country}, {selectedJob.nature_of_job}</p>
+              <div className='flex items-center justify-between mb-4'>
+                <p className='text-xl'>Posted {formatPostedTime(selectedJob.postedAt)}</p>
+                <div className='flex gap-2'>
+                  <button type='button' className='border_btn'>
+                    Share Job
+                  </button>
+                  <button type='button' className='border_btn' onClick={() => setApplyModal(true)}>
+                    Apply Now
+                  </button>
+                </div>
+              </div>
+              <hr className='mb-4' />
+              <h1 className='font-bold text-2xl'>Description</h1>
+              <p className='text-xl'>{selectedJob.description}</p>
+            </div>
+          )}
+        </div>
       </div>
 
-    </div>)
+
+      {applyModal && (
+        <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50'>
+          <div className='bg-white p-6 rounded-lg'>
+            <div className='flex justify-end mb-4'>
+              <button
+                type='button'
+                onClick={() => setApplyModal(false)}
+                className='text-gray-500 hover:text-gray-700'
+              >
+                X
+              </button>
+            </div>
+            <div className='flex justify-between gap-1'>
+              <button type='button' className='border_btn'>
+                Fast Apply
+              </button>
+              {!session &&
+              <button type='button' className='border_btn' onClick={()=> setShowSignModal(true)}>
+                Apply With Login
+              </button>}
+            </div>
+          </div>
+        </div>
+      )
+      }
+
+      {showSignModal && <SignUpOptions setShowSignModal={setShowSignModal}/>}
+    </>
+  )
 }
 
 export default Jobs
---
+++ components/SignUpOptions.tsx
@@ -0,0 +1,91 @@
+'use client'
+import { signIn } from 'next-auth/react';
+import React, { useState } from 'react'
+
+interface Props {
+    setShowSignModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const SignUpOptions: React.FC<Props> = ({ setShowSignModal }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    function handleGoogleSignIn() {
+        signIn('google', { redirect: true, callbackUrl: "http://localhost:3000/", prompt: 'consent' })
+
+    }
+    function handleLinkedinSignIn() {
+        signIn('linkedin', { redirect: true, callbackUrl: "http://localhost:3000/", prompt: 'consent' })
+    }
+    function handleMicrosoftSignIn() {
+        signIn('azure-ad', { redirect: true, callbackUrl: "http://localhost:3000/", prompt: 'consent' })
+    }
+    return (
+    <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50'>
+        <div className='bg-white p-6 rounded-lg w-80'>
+            <div className='flex justify-end'>
+                <button
+                    type='button'
+                    onClick={() => setShowSignModal(false)}
+                    className='text-gray-500 hover:text-gray-700'
+                >
+                    X
+                </button>
+            </div>
+            <form>
+                <input
+                    type='text'
+                    placeholder='Username'
+                    required
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className='border-b-2 border-gray-300 mb-4 mt-2 focus:outline-none w-full py-2'
+                />
+                <input
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    className='border-b-2 border-gray-300 mb-4 focus:outline-none w-full py-2'
+                />
+                <button
+                    type='button'
+                    onClick={() => console.log(`Sign up with Credentials ${username} ${password}`)}
+                    className='w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 focus:outline-none'
+                >
+                    Sign In
+                </button>
+            </form>
+            <p className='mt-4 font-bold text-xl text-center'>---OR---</p>
+            <div className='mt-4'>
+                <button
+                    type='button'
+                    onClick={handleGoogleSignIn}
+                    className='w-full bg-red-600 text-white py-2 rounded-lg hover:bg-red-700 focus:outline-none'
+                >
+                    Sign up with Google
+                </button>
+                <button
+                    type='button'
+                    onClick={handleLinkedinSignIn}
+                    className='w-full bg-blue-700 text-white py-2 rounded-lg hover:bg-blue-800 focus:outline-none mt-2'
+                >
+                    Sign up with LinkedIn
+                </button>
+                <button
+                    type='button'
+                    onClick={handleMicrosoftSignIn}
+                    className='w-full bg-green-600 text-white py-2 rounded-lg hover:bg-green-700 focus:outline-none mt-2'
+                >
+                    Sign up with Microsoft
+                </button>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+
+    export default SignUpOptions
