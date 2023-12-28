ae24ca4ea770561a5eac21043daf6683766c1423
Samara Mohsin <Samara Mohsin@DESKTOP-BP4905N>
2023-12-28 21:32:42 +0500

    changes revert back

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import dummyData from "@/lib/jobs";
-import Image from "next/image";
-import ApplyJobModal from '@/components/ApplyJobModal'
-
-export default function Home() {
-  return (
-    <h1>home</h1>
-     )
-}
