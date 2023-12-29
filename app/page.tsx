549a4063f726237a758e1fc148b791694b4c5a3e
Samara Mohsin <Samara Mohsin@DESKTOP-BP4905N>
2023-12-29 09:03:43 +0500

    css implementation in process

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,22 @@ export default function Home() {
   return (
     <>
     {/* <h1>home</h1> */}
-    <div >
+    <div className="flex flex-col" >
+      <div>
+        <div>filters</div>
+      <div>
+      <Image src="/images/thumbnail.png" alt="cover image" width="500" height="300">
+        </Image>
+        {/* <Image src="https://e7.pngegg.com/pngimages/320/804/png-clipart-regulatory-compliance-business-marketing-recruitment-applicant-tracking-system-business-people-logo-thumbnail.png" alt="external image" width="500" height="300">
+        </Image> */}
+      </div>
+      </div>
       <div>
         <h1>Software Engineering Jobs</h1>
+        <div className='bg-white p-6 rounded-lg w-auto'>
         <ul>
           {dummyData.map((job) => (
-            <li key={job.id}>
+            <li key={job.id} className=' bg-white shadow rounded border border-slate-300 border-spacing-2'>
               <strong>{job.title}</strong>
               <p>{job.description}</p>
               <p>
@@ -20,11 +30,8 @@ export default function Home() {
             </li>
           ))}
         </ul>
+        </div>
       </div>
-        <Image src="/images/thumbnail.png" alt="cover image" width="500" height="300">
-        </Image>
-        {/* <Image src="https://e7.pngegg.com/pngimages/320/804/png-clipart-regulatory-compliance-business-marketing-recruitment-applicant-tracking-system-business-people-logo-thumbnail.png" alt="external image" width="500" height="300">
-        </Image> */}
     </div>
   </>
      )
