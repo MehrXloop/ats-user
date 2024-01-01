ddf24661c25bcdd8f7e096a29ee8cfe3b0b351bf
Mehrunissa Pirwani (2303.KHI.CND.007) <mehrunissa.pirwani@xloopdigital.com>
2024-01-01 15:05:45 +0500

    adding application and settings page content

--- app/application/page.tsx
+++ app/application/page.tsx
@@ -1,20 +1,63 @@
+'use client'
 import UserPanel from '@/components/UserPanel'
+import Link from 'next/link'
 import React from 'react'
 
 const Applications = () => {
+
   return (
-    <div className='flex items-start m-4'>
-      <div>
-        <UserPanel/>
-      </div>
-      <div className='mt-6 w-full'>
-        <h1 className='font-bold text-3xl mb-6'>Application</h1>
-        <div className='border-b-2 border-slate-950'></div>
-        <div className='mt-9'>
-        {/* form */}
+    <>
+      <div className='flex items-start m-4'>
+        <div>
+          <UserPanel />
+        </div>
+        <div className='mt-6 w-full'>
+          <h1 className='font-bold text-3xl mb-6'>Application</h1>
+          <div className='border-b-2 border-slate-950'></div>
+          <div className='mt-9'>
+            {/* form */}
+          </div>
         </div>
       </div>
-    </div>
+      <div className="flex flex-col">
+        <div>
+          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-[80%] mx-auto text-left font-light">
+                <thead className="border-b font-medium text-xl dark:border-neutral-500">
+                  <tr>
+                    <th scope="col" className="px-6 py-4">Job Post</th>
+                    <th scope="col" className="px-6 py-4">Position</th>
+                    <th scope="col" className="px-6 py-4">Apply Job</th>
+                    <th scope="col" className="px-6 py-4">Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="whitespace-nowrap px-6 py-4">27-Nov-2023</td>
+                    <td className="whitespace-nowrap px-6 py-4">
+                      <span className='font-bold'>Sales Development Representative</span>
+                      <br />
+                      Lahore, Punjab, Pakistan
+                      <br />
+                      Full Time
+                    </td>
+                    <td className="whitespace-nowrap px-6 py-4">29-Nov-2023</td>
+                    <td className="whitespace-nowrap px-6 py-4">Submitted</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='flex items-center flex-col mt-20'>
+        <h1 className='font-bold text-4xl'>You&apos;re Applied For A Job</h1>
+        <p className='text-center text-xl mt-2'>Here you will find all the jobs where you,ve applied and also check your job <br /> status apply for more jobs that interest you.</p>
+        <Link href='/' className='mt-10 border-2 dark:border-neutral-500 p-2'>Find your dream jobs here</Link>
+      </div>
+
+    </>
   )
 }
 
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,21 +1,50 @@
-import UserPanel from '@/components/UserPanel'
-import React from 'react'
+import UserPanel from '@/components/UserPanel';
+import React from 'react';
 
 const Settings = () => {
   return (
     <div className='flex items-start m-4'>
       <div>
-        <UserPanel/>
+        <UserPanel />
       </div>
       <div className='mt-6 w-full'>
-        <h1 className='font-bold text-3xl mb-6'>Settings</h1>
+        <h1 className='font-bold text-xl mb-6'>Settings</h1>
         <div className='border-b-2 border-slate-950'></div>
         <div className='mt-9'>
-        {/* form */}
+          <h2 className='border-b border-gray-300 pb-2'>Account</h2>
+          <div className='flex items-center mt-4'>
+            <label className='mr-2' htmlFor='email'>
+              Email
+            </label>
+            <input
+              id='email'
+              value='mehrunissa507@gmail.com'
+              className='border border-gray-300 bg-gray-100 px-2 py-1 rounded-md outline-white'
+              readOnly
+            />
+          </div>
+          <div className='flex items-center mt-2'>
+            <p className='text-gray-500 text-sm mr-2'>
+              You can&apos;t change your email; it&apos;s used to sign in and to be contacted by employers
+            </p>
+          </div>
+          <h2 className='border-b border-gray-300 pb-2 mt-4'>Personalization</h2>
+          <div className='flex items-center m-4'>
+            <input type='checkbox' id='checkbox1' className='mr-2' />
+            <label htmlFor='checkbox1'>Make your profile visible to hiring team</label>
+          </div>
+          <div className='flex items-center m-4'>
+            <input type='checkbox' id='checkbox2' className='mr-2' />
+            <label htmlFor='checkbox2'>Receive job suggestions emails</label>
+          </div>
+          <div className='flex items-center m-4'>
+            <input type='checkbox' id='checkbox3' className='mr-2' />
+            <label htmlFor='checkbox3'>Do you want to delete your account?</label>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
